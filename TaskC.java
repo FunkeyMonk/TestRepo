c1942904581b8bc68a2d4311afdd5b05876b3788
JFlem <jordane.fleming.7@gmail.com>
2025-02-03 18:25:05 -0800

    added a comment

--- TaskC.java
+++ TaskC.java
@@ -4,10 +4,12 @@
 //9/24/2024
 public class TaskC {
     public static void main(String[] args) {
-        for(int i = 5; i>0; i--){ //counts down from 5 to 0 to create 5 rows with each row lower being one less than the row above
-            for (int a = 0; a<i; a++){ //uses row # to find how many cheers to put ex. 4th row = 4 cheers
+        for (int i = 5; i > 0; i--) { // counts down from 5 to 0 to create 5 rows with each row lower being one less
+                                      // than the row above
+            for (int a = 0; a < i; a++) { // uses row # to find how many cheers to put ex. 4th row = 4 cheers
                 System.out.print("\\o/");
             }
+            // this is just to make changes
             System.out.println("\n");
         }
     }
